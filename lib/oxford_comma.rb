cd8d457901aa07cac075ea2da7b94e93c6212ad9
Jonathan  Zylberberg <zylberberg.jonathan@gmail.com>
2016-11-12 01:39:57 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    final1 = array.join(",")
+    final1
+  elsif array.length == 2
+    final2 = array.join(" and ")
+  else
+    last = array.pop
+    final3 = array.join(", ")
+    final3 += ", and " + last
+  end
 end
